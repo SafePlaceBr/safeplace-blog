9737eec136542e1ef0288cd76a4754a45b0d72be
Camila Vilarinho <camilaavilarinho@gmail.com>
2020-04-14 08:37:33 -0300

    Update footer - Remove instagram components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,9 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
-    <br />
-    <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
-        <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
-        </span>
+        <span>© {new Date().getFullYear()} SafePlace</span>
       </div>
     </footer>
   </div>
