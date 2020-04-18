a10a920e7c05fbcd24b41022ae62a6e12257b709
Camila Vilarinho <camilaavilarinho@gmail.com>
2020-04-18 17:36:19 -0300

    Update project name

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,15 +52,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'safeplace',
+        short_name: 'safeplace',
         start_url: '/',
-        background_color: '#00C2BD',
-        theme_color: '#00C2BD',
+        background_color: '#C1272D',
+        theme_color: '#C1272D',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: `${__dirname}/static/images/logo.svg` // This path is relative to the root of the site.
+        icon: `${__dirname}/static/images/logo.png` // This path is relative to the root of the site.
       }
     },
 
