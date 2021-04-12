76c47b6b6db6bde1aa21126c105cff0db35a8971
Camila Vilarinho <camilaavilarinho@gmail.com>
2021-04-12 08:54:47 -0300

    Add social icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Home.css'
 import SubscriptionForm from './SubscriptionForm'
+import Linkedin from 'react-feather/dist/icons/linkedin'
+import Instagram from 'react-feather/dist/icons/instagram'
 
 const Home = () => (
   <div className="home">
@@ -14,6 +16,24 @@ const Home = () => (
       Uma plataforma construída por mulheres e para mulheres
     </p>
     <SubscriptionForm />
+    <div className="socialIcons">
+      <a
+        href="https://www.linkedin.com/company/safeplacebrasil"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="socialIcon"
+      >
+        <Linkedin />
+      </a>
+      <a
+        href="https://instagram.com/safeplace_brasil?igshid=1a5l7sr77ft3w"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="socialIcon"
+      >
+        <Instagram />
+      </a>
+    </div>
   </div>
 )
 
